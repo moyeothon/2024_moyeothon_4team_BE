9bb3bf861197420458a219f0b6c851f23a500c41
sumin1012 <ksm2521101@naver.com>
2024-10-30 00:57:37 +0900

    first

---
+++ src/test/java/com/example/tomorrow_letter/TomorrowLetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tomorrow_letter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomorrowLetterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
